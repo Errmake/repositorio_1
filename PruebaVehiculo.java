7ec2b337c203fd67333c6cb2341e79656f5b1c2c
Errmake <make9m9@gmail.com>
2019-02-04 19:09:42 +0100

    Creados Vehiculo y PruebaVehiculo.

---
+++ PruebaVehiculo.java
@@ -0,0 +1,9 @@
+public class PruebaVehiculo {
+
+	public static void main(String[] args) {
+	Vehiculo v1=new Vehiculo(4,180,3500); //(ruedas,vel,peso)
+
+	System.out.println("Los datos del vehiculo 1 son: ");
+	v1.informe();
+	}
+}
---
+++ Vehiculo.java
@@ -0,0 +1,12 @@
+public class Vehiculo {
+
+private int numRuedas;
+private int velMax;
+private int peso;
+
+Vehiculo(int ruedas, int velocidad, int weight){
+	this.numRuedas=ruedas;
+	this.velMax=velocidad;
+	this.peso=weight;
+}
+}
